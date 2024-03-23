f1f5eb0ebdbb2290d61cca6bb8bc00e91688f132
Alfred-Onuada <aonuada5@gmail.com>
2024-03-23 14:47:22 +0100

    update

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -2,16 +2,32 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Title } from '@angular/platform-browser';
 import { HeaderComponent } from "../../header/header.component";
+import { Router } from '@angular/router';
+import { CreateHallComponent } from "../create-hall/create-hall.component";
+import { CreateRoomComponent } from "../create-room/create-room.component";
+import { GetComplaintsHallComponent } from "../get-complaints-hall/get-complaints-hall.component";
+import { GetComplaintsRoomComponent } from "../get-complaints-room/get-complaints-room.component";
+import { GetHallsComponent } from "../get-halls/get-halls.component";
+import { GetRoomsComponent } from "../get-rooms/get-rooms.component";
+import { GetStudentsInHallComponent } from "../get-students-in-hall/get-students-in-hall.component";
 
 @Component({
     selector: 'app-admin-dashboard',
     standalone: true,
     templateUrl: './admin-dashboard.component.html',
     styleUrls: ['./admin-dashboard.component.css'],
-    imports: [CommonModule, HeaderComponent]
+    imports: [CommonModule, HeaderComponent, CreateHallComponent, CreateRoomComponent, GetComplaintsHallComponent, GetComplaintsRoomComponent, GetHallsComponent, GetRoomsComponent, GetStudentsInHallComponent]
 })
 export class AdminDashboardComponent {
-  constructor(private titleService: Title) {
+  currentPage: string = '';
+
+  constructor(
+    private titleService: Title,
+    private router: Router
+  ) {
     this.titleService.setTitle('Admin Dashboard');
+
+    this.currentPage = typeof this.router.url.split('#')[1] === 'undefined' ? 'home' : this.router.url.split('#')[1];
+    console.log(this.currentPage)
   }
 }
